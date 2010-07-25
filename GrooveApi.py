09989cb77561edcdda73ae305b463b59c132d4e4
jlew <jlew.blackout@gmail.com>
2010-07-25 19:06:25 -0400

    Improved grooveshark api implemenation

---
+++ GrooveApi.py
@@ -0,0 +1,359 @@
+# Groove Api
+#
+# Api Documentation:
+#    http://grooveshark.wikia.com/wiki/External_Player_Control_API_Docs
+#
+#
+from json import load
+from urllib import urlopen, quote_plus
+from os import stat
+
+class GrooveApi:
+    def __init__(self, file_path, remote_path):
+        self.__file_path = file_path
+        self.__remote_path = remote_path
+        self.__file_time = 0
+        self.__last_mode = "stopped"
+
+        self.__queue = []
+        self.__played = []
+        self.__song_db = {}
+
+    @property
+    def played(self):
+        return self.__played[:]
+
+    @property
+    def queue(self):
+        return self.__queue[:]
+
+    @property
+    def song_db(self):
+        return self.__song_db.copy()
+
+    def get_file_status(self, force=False):
+        new_time = stat(self.__file_path).st_mtime
+
+        # Read file if time is newer
+        if new_time > self.__file_time or force:
+            try:
+                f = open(self.__file_path, 'r')
+                fc = f.read().split('\t')
+                f.close()
+                self.__file_time = new_time
+            except:
+                pass
+
+            self.__last_mode = fc[3]
+            return {
+                    'title': fc[0],
+                    'artist': fc[1],
+                    'album': fc[2],
+                    'status': fc[3]
+                    }
+
+        else:
+            return False
+
+    def request_song_from_api(self, search):
+        url = "http://tinysong.com/b/%s?format=json" % quote_plus(search)
+
+        try:
+            data = load(urlopen(url))
+        except:
+            return False
+
+        if len(data):
+            self.__song_db[data['SongID']] = data
+
+        return data
+
+    #TO BE REMOVED ONCE UPDATE ADDED
+    def queue_song(self, id):
+        if self.__last_mode == "stopped":
+            self.play_song(id)
+        else:
+            self.__queue.append(id)
+
+    #TO BE REMOVED ONCE UPDATE ADDED
+    def play_song(self, id):
+        self.__played.append(id)
+        self.api_play_id(id)
+
+    #TO BE REMOVED ONCE UPDATE ADDED
+    def auto_play(self):
+        try:
+            id = self.__queue.pop(0)
+            self.play_song(id)
+        except:
+            pass
+
+    #TO BE REMOVED ONCE UPDATE ADDED
+    def remove_queue(self, id):
+        try:
+            self.__queue.remove(id)
+            return True
+        except:
+            return False
+
+    ###### API CALLS #######
+    def api_pause(self):
+        """
+        Pauses the current song
+        Does nothing if no song is playing
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('pause')
+
+    def api_play(self):
+        """
+        Plays the current song
+        If the current song is paused it resumes the song
+        If no songs are in the queue, it does nothing
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('play')
+
+    def api_play_pause(self):
+        """
+        Toggles between paused and playing
+        Scenarios of current song
+        Not Playing: Plays the song
+        Paused: Resumes playback
+        Playing: Pauses song
+        If no songs are in the queue, it does nothing
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('playpause')
+
+    def api_next(self):
+        """
+        Plays the next song in the queue
+        If no songs are left it does nothing
+        """
+        self.__send_api('next')
+
+    def api_previous(self):
+        """
+        Plays the previous song in the queue
+        If shuffle is enabled, it will play the last song played.
+        If no songs are left, it will do nothing.
+        If the current song has been playing for over 5 seconds, it
+        will restart the song.
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('previous')
+
+    def api_previous_song(self):
+        """
+        Behaves like previous except it will ALWAYS play the
+        previous song.
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('previoussong')
+
+    def api_stop(self):
+        """
+        Stops playback
+        If no songs are in the queue, it does nothing
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('stop')
+
+    #mute
+    #Toggles mute playback
+    #If player is muted, it will mute
+    #If player is not muted, it will disable mute
+    #Requires version: v2.0 r20100518.2
+
+    def api_volume_up(self):
+        """
+        Increases the volume by 20%
+        If the volume is >100% it will do nothing
+        If the volume is 0 it will set the volume to 10%
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('volumeup')
+
+    def api_volume_down(self):
+        """
+        Decreases the volume by 20%
+        Will continue to go to 0 indefinitely.
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('volumedown')
+
+    def api_shuffle(self):
+        """
+        Toggles between shuffle on/off
+        Shuffle is persistent as of v2.0 r20100518.2 and this command
+        will affect global shuffle setting
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('shuffle')
+
+    def api_clear_queue(self):
+        """
+        Clears the queue of all the songs.
+        This will also reset the repeat mode
+        If the queue is already cleared, this will do nothing
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('clearqueue')
+
+    def api_add_favorite(self):
+        """
+        Favorites the current song
+        This will add it to your library as well
+        If the song is already favorited, this will do nothing
+        If no song is playing or selected in the queue, nothing
+        will happen
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('favorite')
+
+    def api_remove_favorite(self):
+        """
+        If the current song is favorited, it will unfavorite it
+        If the song is not favorited, this will do nothing
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('unfavorite')
+
+    def api_toggle_favorite(self):
+        """
+        Toggles the favorite status of the current song
+        Requires version: v2.0 r20100518.2
+        """
+        self.__send_api('togglefavorite')
+
+    #showsongtoast
+    #Shows a lightbox-style box with the information of the currently
+    #selected song or currently playing song
+    #If no song is playing or selected, this will do nothing.
+    #Requires version: v2.0 r20100518.2
+
+
+    def api_toggle_radio(self):
+        """
+        Toggles the status of radio
+        If radio is off, it will be turned on
+        If radio is on, it will be turned off
+        If nothing is in the queue, radio will not turn on and a
+        message will be presented saying no songs were found.
+        Requires version: v2.0 r20100518.5
+        """
+        self.__send_api('radio')
+
+    def api_radio_on(self):
+        """
+        If radio is off, it will be turned on
+        Requires version: v2.0 r20100518.5
+        """
+        self.__send_api('radioon')
+
+    def api_radio_off(self):
+        """
+        If radio is on, it will be turned off
+        Requires version: v2.0 r20100518.5
+        """
+        self.__send_api('radiooff')
+
+    def api_radio_vote_up(self):
+        """
+        If radio is on, then the current song will receive a smile (like)
+        If no song is selected or playing, this will do nothing
+        If radio is off, this will do nothing
+        Requires version: v2.0 r20100518.5
+        """
+        self.__send_api('smile')
+
+    def api_radio_vote_down(self):
+        """
+        If radio is on, then the current song will receive a frown
+        (dislike).
+        After being frowned, a song will stop playing and the next
+        song will begin playing.
+        Requires version: v2.0 r20100518.5
+        """
+        self.__send_api('frown')
+
+    #togglesmile
+    #Toggles the smile status of the current song.
+    #Requires version: v2.0 r20100518.5
+    #togglefrown
+    #Toggles the frown status of the current song
+    #Requires version: v2.0 r20100518.5
+
+    #repeat
+    #If there are songs in the queue, it will change the repeat
+    #setting of the current queue.
+    #If no queue is present, nothing will happen.
+    #This method will toggle between Repeat All, Repeat One, Repeat None.
+    #Coming soon.
+
+    def api_addtolibrary(self):
+        """
+        Adds the currently playing song to the users library
+        If no song is playing, this will do nothing.
+        Coming soon.
+        """
+        self.__send_api('addtolibrary')
+
+    ###### ARG-API CALLS ######
+    def api_play_id(self, id):
+        """
+        * Plays the song specified by songID
+        * Stops the currently playing song, if one is playing
+        * Adds the song specified to the queue in the next position
+        * If the song specified already exists in the queue, the song
+          will be selected and played. It will not be added.
+        * If songID is invalid, nothing will happen.
+        """
+        self.__send_api('playsong %d' % id)
+
+    def api_play_token(self, token):
+        """
+        * Plays the song specified by songToken
+        * Follows the same rules as playsong.
+        """
+        self.__send_api('playsongex %s' % token)
+
+    def api_queue_id(self, id):
+        """
+        * Adds the song specified by songID to the end of the queue
+        * Follows the same rules as addsong.
+        """
+        self.__send_api('addsongfromid %d' % id)
+
+    def api_queue_token(self, token):
+        """
+        * Adds the song specified by songToken to the end of the queue
+        * If the song is already in the queue, it WILL be added again.
+        * If songToken is invalid, nothing will happen.
+        """
+        self.__send_api('addsong %s' % token)
+
+    def api_queue_next_id(self, id):
+        """
+        Adds the song specified by songID right after the playing song
+        (or "next")
+        Follows the same rules as addsongfromid.
+        """
+        self.__send_api('addsongnextfromid %d' % id)
+
+    def api_queue_next_token(self, token):
+        """
+        Adds the song specified by songToken right after the playing song
+        (or "next")
+        Follows the same rules as addsong.
+        """
+        self.__send_api('addsongnext %s' % token)
+
+    def __send_api(self, text):
+        """
+        Sends the command to the api file
+        """
+        f = open(self.__remote_path, 'a')
+        f.write("%s\n" % text)
+        f.close()
--- ircgroove.py
+++ ircgroove.py
@@ -2,27 +2,19 @@
 from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
 
+from GrooveApi import GrooveApi
+
 from JlewBot import JlewBotFactory
 
-import os
+api_inst = GrooveApi('/home/jlew/Documents/Grooveshark/currentSong.txt',
+'/home/jlew/.appdata/GroovesharkDesktop.7F9BF17D6D9CB2159C78A6A6AB076EA0B1E0497C.1/Local Store/shortcutAction.txt')
 
-#UGLY FOR NOW
-from json import load
-from urllib import urlopen, quote_plus
-
-file_name = '/home/jlew/Documents/Grooveshark/currentSong.txt'
-file_time = 0
-last_mode = "stopped"
-
-queue = []
-played = []
-song_db = {}
-
+import unicodedata
 def convert_to_ascii(data):
     try:
-        d = str( data )
+        d = str(data)
     except:
-        d = "UNICODE"
+        d = unicodedata.normalize('NFKD', data).encode('ascii','ignore')
     return d
 
 def check_file_status(irc_bot, channel):
@@ -30,83 +22,42 @@ def check_file_status(irc_bot, channel):
         irc_bot = irc_bot.active_bot
     else:
         return
-    global file_time
-    global last_mode
-    global file_name
-    new_time = os.stat( file_name ).st_mtime
 
-    if new_time > file_time:
-        try:
-            f = open( file_name, 'r' )
-            fc = f.read().split('\t')
-            f.close()
-            file_time = new_time
-        except:
-            pass
-        msg = "%s: \"%s\" by \"%s\" on \"%s\"" % (convert_to_ascii(fc[3]), convert_to_ascii(fc[0]), convert_to_ascii(fc[1]), convert_to_ascii(fc[2]))
-        f.close()
+    global api_inst
+    s = api_inst.get_file_status()
+    if s:
+        msg = "%s: \"%s\" by \"%s\" on \"%s\"" % \
+                (convert_to_ascii(s['status']),
+                 convert_to_ascii(s['title']),
+                 convert_to_ascii(s['artist']),
+                 convert_to_ascii(s['album']))
+
+        if s['status'] == 'stopped':
+            api_inst.auto_play()
 
         irc_bot.me( channel, msg )
 
-        if fc[3] == 'stopped':
-                try:
-                    global queue
-                    id = queue.pop(0)
-                    global played
-                    played.append(id)
-                    play_song( id )
-                except:
-                    pass
-
-        last_mode = fc[3]
-
-def get_song_info(song_search):
-    search_text = quote_plus( song_search )
-    url = "http://tinysong.com/b/%s?format=json" % search_text
-
-    data = load( urlopen( url ) )
-
-    if len(data):
-        global song_db
-        song_db[data['SongID']] = data
-
-    return data
-
-def queue_song(id):
-    global last_mode
-    if last_mode == "stopped":
-        global played
-        played.append(id)
-        play_song(id)
-
-    else:
-        global queue
-        queue.append(id)
-
-def play_song(id):
-    f = open('/home/jlew/.appdata/GroovesharkDesktop.7F9BF17D6D9CB2159C78A6A6AB076EA0B1E0497C.1/Local Store/shortcutAction.txt', 'a')
-    #f.write("clearqueue\n")
-    f.write("playsong %d\n" % id)
-    f.close()
-
 def request_queue_song( responder, user, channel, command, msg):
+    global api_inst
     if command == "add":
         responder("Got Request, processing")
-        song_packet = get_song_info(msg)
 
-        global played
-        global queue
+        song_packet = api_inst.request_song_from_api(msg)
 
-        if len( song_packet ) == 0:
+        if not song_packet:
+            responder("API Threw Exception, try again")
+            return
+
+        if len(song_packet) == 0:
             responder("API returned empty set")
 
-        elif song_packet['SongID'] in played:
+        elif song_packet['SongID'] in api_inst.played:
             responder("Error \"%s\" by \"%s\" has been played" % (\
                 convert_to_ascii(song_packet['SongName']),
                 convert_to_ascii(song_packet['ArtistName']),
                 ))
 
-        elif song_packet['SongID'] in queue:
+        elif song_packet['SongID'] in api_inst.queue:
             responder("Error \"%s\" by \"%s\" is in queue" % (\
                 convert_to_ascii(song_packet['SongName']),
                 convert_to_ascii(song_packet['ArtistName']),
@@ -118,36 +69,51 @@ def request_queue_song( responder, user, channel, command, msg):
                 convert_to_ascii(song_packet['ArtistName']),
                 convert_to_ascii(song_packet['AlbumName'])
                 ))
-            queue_song( song_packet['SongID'] )
+            api_inst.queue_song( song_packet['SongID'] )
 
     elif command == "remove":
-        global queue
         try:
             id = int(msg)
-            queue.remove( id )
-            try:
-                global song_db
-                name = "%s by %s" %(convert_to_ascii(song_db[id]['SongName']) , convert_to_ascii(song_db[id]['ArtistName']))
-            except:
-                name = msg
-            responder( "Removed %s" % name )
+            if api_inst.remove_queue(id):
+                responder("Removed %s" % id)
+            else:
+                responder( "Could not remove %s" % msg )
+
         except:
-            responder( "Could not remove %s" % msg )
+            responder( "Must get an id" )
 
     elif command == "show":
-        global queue
-        global song_db
         songNames = []
-        for id in queue:
+        song_db = api_inst.song_db
+        for id in api_inst.queue:
             songNames.append( "%s by %s" %(convert_to_ascii(song_db[id]['SongName']), convert_to_ascii(song_db[id]['ArtistName'])))
         responder(", ".join(songNames))
 
+    elif command == "pause":
+        api_inst.api_pause()
+
+    elif command == "resume":
+        api_inst.api_play()
+
+    elif command == "skip":
+        api_inst.api_stop()
+
+        # this is a hack, stop so that we autoqueue
+        #api_inst.api_next()
+
+    elif command == "volup":
+        api_inst.api_volume_up()
+        responder("ACK")
+
+    elif command == "voldown":
+        api_inst.api_volume_down()
+        responder("ACK")
 
 if __name__ == '__main__':
     f = JlewBotFactory("#rit-groove", name="foss_groovebot")
-    f.register_command('add', request_queue_song)
-    f.register_command('remove', request_queue_song)
-    f.register_command('show', request_queue_song)
+
+    for command in ['add','remove','show','pause','resume','skip','volup','voldown']:
+        f.register_command(command, request_queue_song)
     reactor.connectTCP("irc.freenode.net", 6667, f)
 
     lc = LoopingCall(check_file_status, f, "#rit-groove").start( 2 )
