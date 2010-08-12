b12e5baf9de5fe612b8ce7bbeb634e92ca08d831
Nathaniel Case <Qalthos@gmail.com>
2010-08-12 11:17:52 -0400

    Added some more command handling and cleaned up some of the branches.

--- bot.py
+++ bot.py
@@ -96,39 +96,49 @@ class GrooveClientFactory(ClientFactory):
             
         if len(msg_parts) < 2:
             return
+
         command = msg_parts[1]
+        ignored = ["Got", "Available", "Error", "Command"]
 
         # This has the annoying habit of returning a second entry of an empty
         # string on empty queues.  If this happens, just return, we don't care
         # about it.
         if command == "":
-            return
-            
-        if command == "stopped":
-            self.gui.remove(text=msg_parts[2])
-        elif command == "loading":
             pass
-        elif command == "playing":
+        # Current song modifiers
+        elif command == "stopped":
+            self.gui.now_playing("")
+        elif command == "loading":
+            # We can't rely on recieving a loading cue.
+            pass
+        elif command in ["playing", "paused"]:
+            self.gui.remove(text=msg_parts[2])
             self.gui.pause_toggle(command)
             self.gui.now_playing(msg_parts[2])
-        elif command == "paused":
-            self.gui.pause_toggle(command)
-            self.gui.now_playing(msg_parts[2])
+        # Volume handlers
         elif command.split()[0] == "OK":
             if len(command) > 3:
                 #Volume has changed
                 volume = command.split()[1]
                 self.gui.volmod(volume)
-            # Implicit else ignore
+        elif len(command) <= 3:
+            self.gui.volmod(command)
+        # Modifications to the queue
         elif command.split()[0] == "Queueing":
             self.gui.add(user, command.split()[1], msg_parts[2])
         elif command.split()[0] == "Removed":
             removed_id = command.split()[1]
             self.gui.remove(song_id=removed_id)
-            return
-        elif command.split()[0] == "Got" or command.split()[0] == "Available" or command.split()[0] == "Error":
-            # Ignore these replies.
-            return
+        # General responses we can ignore
+        elif command.split()[0] in ignored:
+            pass
+        # More complex queue actions
+        elif len(command.split()) > 1 and command.split()[1][:1] == "[":
+            # Uhhh... hopefully its a dump...
+            song_info = command.split()
+            song_name = msg_parts[2]
+            song_info.append(song_name)
+            self.gui.add(song_info[1][1:len(song_info[1])-1], song_info[0], song_info[2])
         else:
             # Maybe it's a show response?
             songs = command.split(", ")
