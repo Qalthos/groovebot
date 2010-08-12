2a3ff6e3c9357f38053936ef6ea0bede89b95e58
Nathaniel Case <Qalthos@gmail.com>
2010-08-12 13:37:22 -0400

    Add some search by text and cleaned up the methods somewhat.

--- gui.py
+++ gui.py
@@ -27,10 +27,8 @@ class GrooveGui:
     def add(self, requester, song_id, text):
         # We want to ignore songs we've already seen, but they don't
         # always come the same way.
-        for stored_id, song in self.queue_keeper.items():
-            # Since the album is what generally gets lost, look for the front.
-            if song["text"].startswith(text) or text.startswith(song["text"]):
-                return
+        if self._queue_search(text):
+            return
         
         song = gtk.Label("%s requested %s" % (requester, text))
         self._queue_box.add(song)
@@ -38,22 +36,39 @@ class GrooveGui:
         song.show()
         self.queue_keeper[song_id] = {"requester": requester, "label": song, "text": text}
 
-    def remove(self, song_id="", text=""):
-        if not song_id == "":
-            self._queue_box.remove(self.queue_keeper[song_id]["label"])
-            return
-        elif not text == "":
-            for id, song in self.queue_keeper.items():
-                if text.startswith(song["text"]):
-                    self._queue_box.remove(song["label"])
-                    return
-        print "Couldn't remove %s%s" % (song_id, text)
+    def remove(self, song_id=None, text=None):
+        if not song_id and text:
+            song = self._queue_search(text)
+            if not song:
+                print "Couldn't remove %s %s" % (song_id, text)
+                return
+        elif song_id:
+            song = self.queue_feeper[song_id]
+        
+        self._queue_box.remove(song["label"])
 
     def volmod(self, new_vol):
         self._volume.set_text(new_vol)
 
-    def now_playing(self, text):
-        self._now_playing.set_text("Now Playing: %s" % text)
+    def now_playing(self, text=None, song=None):
+        if text == "":
+            self._now_playing.set_text("No song playing.")
+            return
+            
+        if not song and text:
+            song = self._queue_search(text)
+            if not song:
+                self.add("someone", "???", text)
+                song = self.queue_keeper["???"]
+        self._now_playing.set_text("Now Playing: %s\nRequested by %s" % (song["text"], song["requester"]))
+
+    def pause_toggle(self, action):
+        if action == "playing":
+            self._resume.hide()
+            self._pause.show()
+        if action == "paused":
+            self._pause.hide()
+            self._resume.show()
 
     def groove(self, factory):
         self.client_factory = factory
@@ -65,15 +80,13 @@ class GrooveGui:
         widget_name = gtk.Buildable.get_name(widget)
         self.client_factory.active_bot.talk(widget_name)
 
-    def pause_toggle(self, action):
-        if action == "playing":
-            self._resume.hide()
-            self._pause.show()
-        if action == "paused":
-            self._pause.hide()
-            self._resume.show()
-
     def quit(self, widget):
         reactor.callLater(0.5, reactor.stop)
         #reactor.stop()
         gtk.main_quit()
+
+    def _queue_search(self, text):
+        for stored_id, song in self.queue_keeper.items():
+            # Since the album is what generally gets lost, look for the front.
+            if song["text"].startswith(text) or text.startswith(song["text"]):
+                return song
