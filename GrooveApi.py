bb05402b0ec0c5e7293e280f9b61233fcc2ab303
jlew <jlew.blackout@gmail.com>
2010-10-01 14:41:21 -0400

    Added gpl header

--- GrooveApi.py
+++ GrooveApi.py
@@ -3,7 +3,22 @@
 # Api Documentation:
 #    http://grooveshark.wikia.com/wiki/External_Player_Control_API_Docs
 #
+#    This file is part of GrooveBot.
 #
+#    GrooveBot is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    GrooveBot is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with OpenVideoChat.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from json import load
 from urllib import urlopen, quote_plus
 from os import stat
--- JlewBot.py
+++ JlewBot.py
@@ -1,3 +1,18 @@
+#    JlewBot is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    JlewBot is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with JlewBot.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 from twisted.words.protocols.irc import IRCClient
 from twisted.internet.protocol import ClientFactory
 from twisted.internet import reactor
