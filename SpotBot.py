4a654666e1e0b132677d9605e47162bdfbe0eed0
Nathaniel Case <Qalthos@gmail.com>
2011-10-20 11:22:51 -0400

    Spotify backend.  Wooooo!

---
+++ SpotBot.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+#    This file is part of GrooveBot.
+#
+#    GrooveBot is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    GrooveBot is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with GrooveBot.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import unicodedata
+
+from twisted.internet import reactor, threads, utils
+from twisted.internet.task import LoopingCall
+
+from SpotApi import SpotApi
+
+from JlewBot import JlewBotFactory
+from VolBot import VolBot
+
+
+class SpotBot(VolBot):
+    bot_name = "foss_spotbot"
+    song_request_db = {}
+
+    def setup(self, f, uname, upass):
+        #super(SpotBot, self).setup(f)
+        f.register_command('vol', self.volume_change)
+        f.register_command('source', self.simple_response)
+        reactor.callWhenRunning(self._set_vol)
+        # ^ super?
+
+        for command in ['add','remove','show','pause','resume','skip','status','dump','radio']:
+            f.register_command(command, self.request_queue_song)
+        self.api_inst = SpotApi(uname, upass)
+
+    def convert_to_ascii(self, data):
+        try:
+            d = str(data)
+        except:
+            d = unicodedata.normalize('NFKD', data).encode('ascii','ignore')
+        return d
+
+    # I still don't know what this does for GrooveBot
+    def _file_status(self, s, bot, channel):
+        if s:
+            msg = "%s: \"%s\" by \"%s\" on \"%s\"" % \
+                    (self.convert_to_ascii(s['status']),
+                     self.convert_to_ascii(s['title']),
+                     self.convert_to_ascii(s['artist']),
+                     self.convert_to_ascii(s['album']))
+
+            if s['status'] == 'stopped':
+                threads.deferToThread(self.api_inst.auto_play).addErrback(self.err_console)
+            bot.me(channel, msg)
+
+    def _add_lookup_cb(self, song_packet, responder, user):
+        if not song_packet:
+            responder("No songs found.")
+
+        elif song_packet['SongID'] in self.api_inst.queue:
+            responder('"%s" by "%s" is already in queue' % (\
+                self.convert_to_ascii(song_packet['SongName']),
+                self.convert_to_ascii(song_packet['ArtistName']),
+                ))
+        else:
+            responder('Queueing %s: "%s" by "%s" on "%s"' % (\
+                song_packet['SongID'],
+                self.convert_to_ascii(song_packet['SongName']),
+                self.convert_to_ascii(song_packet['ArtistName']),
+                self.convert_to_ascii(song_packet['AlbumName'])
+                ))
+            self.song_request_db[song_packet['SongID']] = user
+            self.api_inst.queue_song(song_packet['SongID'])
+
+    def request_queue_song(self, responder, user, channel, command, msg):
+        if channel == self.bot_name and not command in ['show', 'dump', 'status', 'vol']:
+            responder("Let's talk to the class")
+            return
+        if command == "add":
+            responder("Got Request, processing")
+            threads.deferToThread(self.api_inst.request_song_from_api, msg).addCallback(self._add_lookup_cb, responder, user).addErrback(self.err_chat, responder)
+
+        elif command == "remove":
+            if self.api_inst.remove_queue(msg):
+                responder('Removed %s' % msg)
+            else:
+                responder('Could not remove %s' % msg)
+
+        elif command == "show":
+            songNames = []
+            song_db = self.api_inst.song_db
+            for id in self.api_inst.queue:
+                songNames.append('"%s" by "%s"' %(self.convert_to_ascii(song_db[id]['SongName']), self.convert_to_ascii(song_db[id]['ArtistName'])))
+            responder(', '.join(songNames))
+
+        elif command == "dump":
+            song_db = self.api_inst.song_db
+            for id in self.api_inst.queue:
+                responder('%s [%s]: "%s" by "%s" on "%s"' % ( id,
+                    self.song_request_db[id],
+                    self.convert_to_ascii(song_db[id]['SongName']),
+                    self.convert_to_ascii(song_db[id]['ArtistName']),
+                    self.convert_to_ascii(song_db[id]['AlbumName'])))
+
+        elif command == "pause":
+            threads.deferToThread(self.api_inst.api_pause).addCallback(self.ok, responder).addErrback(self.err_chat, responder)
+
+        elif command == "resume":
+            threads.deferToThread(self.api_inst.api_play).addCallback(self.ok, responder).addErrback(self.err_chat, responder)
+
+        elif command == "skip":
+            threads.deferToThread(self.api_inst.api_next).addCallback(self.ok, responder).addErrback(self.err_chat, responder)
+
+        elif command == "radio":
+            if msg == "on":
+                threads.deferToThread(self.api_inst.api_radio_on).addCallback(self.ok, responder).addErrback(self.err_chat, responder)
+            elif msg == "off":
+                threads.deferToThread(self.api_inst.api_radio_off).addCallback(self.ok, responder).addErrback(self.err_chat, responder)
+
+        elif command == "status":
+            song = self.api_inst.current_song
+            responder('"%s" by "%s"' %(self.convert_to_ascii(song['SongName']), self.convert_to_ascii(song['ArtistName'])))
+
+
+if __name__ == '__main__':
+    if not len(sys.argv) == 3:
+        sys.exit()
+    bot = SpotBot()
+    f = JlewBotFactory(protocol=SpotBot)
+    bot.setup(f, sys.argv[1], sys.argv[2])
+    reactor.connectTCP("irc.freenode.net", 6667, f)
+    reactor.run()
