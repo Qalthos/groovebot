9783a2c396e65a7a99fa68a859e7c18e6bfe1efa
Nathaniel Case <Qalthos@gmail.com>
2011-10-24 16:06:11 -0400

    fix imports.

--- SpotBot.py
+++ SpotBot.py
@@ -14,6 +14,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with GrooveBot.  If not, see <http://www.gnu.org/licenses/>.
 
+from getpass import getpass
 import sys
 import unicodedata
 
