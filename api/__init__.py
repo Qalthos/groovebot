5bc31e8bf976f6165d798b5a9afe11ceff271107
Nathaniel Case <Qalthos@gmail.com>
2015-04-15 19:18:18 -0400

    Remove queue details from gst.

--- api/__init__.py
+++ api/__init__.py
@@ -38,18 +38,6 @@ class GstPlayerAPI(object):
         self.__player.set_property("uri", uri)
         self.__player.set_state(gst.STATE_PLAYING)
 
-    def auto_play(self):
-        bus = self.__player.get_bus()
-        while bus.have_pending():
-            self.on_message(bus, bus.pop())
-        if self.__last_mode == 'stopped':
-            song = self.__queue.pop(0)
-            self.play_song(song.audioUrl)
-            self.__current = song
-            # Pull songs so we don't have to fetch them later
-            if len(self.__queue) <= 2:
-                self.__queue.extend(self.__station.get_playlist())
-
     ##### API CALLS #####
     def api_pause(self):
         """
@@ -57,7 +45,6 @@ class GstPlayerAPI(object):
         Does nothing if no song is playing
         """
         self.__player.set_state(gst.STATE_PAUSED)
-        self.__last_mode = 'paused'
 
     def api_play(self):
         """
@@ -65,33 +52,7 @@ class GstPlayerAPI(object):
         If the current song is paused it resumes the song
         If no songs are in the queue, it does nothing
         """
-        if self.__last_mode == 'paused':
-            self.__player.set_state(gst.STATE_PLAYING)
-            self.__last_mode = 'playing'
-        else:
-            self.auto_play()
-
-    def api_play_pause(self):
-        """
-        Toggles between paused and playing
-        Scenarios of current song
-        Not Playing: Plays the song
-        Paused: Resumes playback
-        Playing: Pauses song
-        If no songs are in the queue, it does nothing
-        """
-        if self.__last_mode == 'playing':
-            self.api_pause()
-        elif self.__last_mode == 'paused' or not len(self.__queue) == 0:
-            self.api_play()
-
-    def api_next(self):
-        """
-        Plays the next song in the queue
-        If no songs are left it does nothing
-        """
-        self.api_stop()
-        self.auto_play()
+        self.__player.set_state(gst.STATE_PLAYING)
 
     def api_stop(self):
         """
